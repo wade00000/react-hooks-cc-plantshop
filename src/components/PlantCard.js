c1a21ddd8b0a823fb6ec32231f7c5a786f6d2dc1
WADE <MEANTOINE12@GMAIL.COM>
2025-09-14 18:36:35 +0300

    fetched plants are set in state and rendered

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+  const {name,image,price} = plant
+  
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [plants,setPlants] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/plants")
+      .then(r => r.json())
+      .then(data => setPlants(data))
+  })
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plants.map(plant=>(
+      <li key={plant.id}><PlantCard plant={plant}/></li>
+    ))}</ul>
   );
 }
 
